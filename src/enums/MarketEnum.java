51cd17cf64bf18944e5d92e455ea3c2f195d442c
Kris Kristensen <KRKR1303@dsb.dk>
2019-12-18 13:39:01 +0100

    Updated test with marketing checks

--- src/enums/MarketEnum.java
+++ src/enums/MarketEnum.java
@@ -4,6 +4,7 @@ public enum MarketEnum {
 
     TALON_KNIFE_FADE("market/listings/730/%E2%98%85%20Talon%20Knife%20%7C%20Fade%20%28{}%29"),
     TALON_KNIFE_DOPPLER("market/listings/730/%E2%98%85%20Talon%20Knife%20%7C%20Doppler%20%28{}%29"),
+    AK47_WILD_LOTUS("market/listings/730/AK-47%20%7C%20Wild%20Lotus%20%28{}%29"),
     AWP_ASIIMOV_STATTRAK("market/listings/730/StatTrak%E2%84%A2%20AWP%20%7C%20Asiimov%20%28{}%29"),
     AWP_DRAGON_LORE("market/listings/730/AWP%20|%20Dragon%20Lore%20%28{}%29"),
     AWP_THE_PRINCE("market/listings/730/AWP%20%7C%20The%20Prince%20%28{}%29"),
--- src/model/Connector.java
+++ src/model/Connector.java
@@ -1,7 +1,6 @@
 package model;
 
 import enums.ClientAuth;
-import model.Request;
 
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -11,7 +10,7 @@ import java.util.Base64;
 public class Connector {
 
     private final int CONNECTION_TIMEOUT = 30000;
-    private final int READ_TIMEOUT = 60000;
+    private final int READ_TIMEOUT = 30000;
 
     private String username, password;
 
@@ -31,45 +30,11 @@ public class Connector {
         try {
             long start = System.currentTimeMillis();
 
-            URL url = new URL(request.getServer() + restPath);
-            HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+            HttpURLConnection httpCon = createHttpURLConnection(request, restPath, "POST", true);
 
-            addAuthorization(httpCon, username, password);
-
-            httpCon.setDoOutput(true);
-            httpCon.setRequestMethod("POST");
-            httpCon.setRequestProperty("accept-charset", "UTF-8");
-            httpCon.setRequestProperty("content-type", "application/xml");
-            httpCon.setConnectTimeout(CONNECTION_TIMEOUT);
-            httpCon.setReadTimeout(READ_TIMEOUT);
-
-            if (request.getXml() != null) {
-                OutputStream os = httpCon.getOutputStream();
-                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
-
-                bw.write(request.getXml());
-                bw.flush();
-                bw.close();
-            }
-
-            request.setResponseCode(httpCon.getResponseCode());
-            request.setResponseMessage(httpCon.getResponseMessage());
-
-            StringBuilder sb = new StringBuilder();
-            InputStream iStream = httpCon.getErrorStream() == null ? httpCon.getInputStream() : httpCon.getErrorStream();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(iStream, "UTF-8"));
-            String brLine = br.readLine();
-
-            while (brLine != null) {
-                sb.append(brLine);
-                brLine = br.readLine();
-            }
-
-            br.close();
+            readInput(request, httpCon);
 
             request.setResponseTime(System.currentTimeMillis() - start);
-            request.setResponseXml(sb.toString());
 
             return request.getResponseXml();
         } catch (IOException e) {
@@ -83,45 +48,11 @@ public class Connector {
         try {
             long start = System.currentTimeMillis();
 
-            URL url = new URL(request.getServer() + restPath);
-            HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+            HttpURLConnection httpCon = createHttpURLConnection(request, restPath, "PUT", true);
 
-            addAuthorization(httpCon, username, password);
-
-            httpCon.setDoOutput(true);
-            httpCon.setRequestMethod("PUT");
-            httpCon.setRequestProperty("accept-charset", "UTF-8");
-            httpCon.setRequestProperty("content-type", "application/xml");
-            httpCon.setConnectTimeout(CONNECTION_TIMEOUT);
-            httpCon.setReadTimeout(READ_TIMEOUT);
-
-            if (request.getXml() != null) {
-                OutputStream os = httpCon.getOutputStream();
-                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
-
-                bw.write(request.getXml());
-                bw.flush();
-                bw.close();
-            }
-
-            request.setResponseCode(httpCon.getResponseCode());
-            request.setResponseMessage(httpCon.getResponseMessage());
-
-            StringBuilder sb = new StringBuilder();
-            InputStream iStream = httpCon.getErrorStream() == null ? httpCon.getInputStream() : httpCon.getErrorStream();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(iStream, "UTF-8"));
-            String brLine = br.readLine();
-
-            while (brLine != null) {
-                sb.append(brLine);
-                brLine = br.readLine();
-            }
-
-            br.close();
+            readInput(request, httpCon);
 
             request.setResponseTime(System.currentTimeMillis() - start);
-            request.setResponseXml(sb.toString());
 
             return request.getResponseXml();
         } catch (IOException e) {
@@ -135,35 +66,11 @@ public class Connector {
         try {
             long start = System.currentTimeMillis();
 
-            URL url = new URL(request.getServer() + restPath);
-            HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+            HttpURLConnection httpCon = createHttpURLConnection(request, restPath, "GET", false);
 
-            addAuthorization(httpCon, username, password);
-
-            httpCon.setRequestMethod("GET");
-            httpCon.setConnectTimeout(CONNECTION_TIMEOUT);
-            httpCon.setReadTimeout(READ_TIMEOUT);
-
-            request.setResponseCode(httpCon.getResponseCode());
-            request.setResponseMessage(httpCon.getResponseMessage());
-            request.clearStrings();
-
-            StringBuilder sb = new StringBuilder();
-            InputStream iStream = httpCon.getErrorStream() == null ? httpCon.getInputStream() : httpCon.getErrorStream();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(iStream, "UTF-8"));
-            String brLine = br.readLine();
-
-            while (brLine != null) {
-                sb.append(brLine);
-                request.appendString(brLine);
-                brLine = br.readLine();
-            }
-
-            br.close();
+            readInput2(request, httpCon);
 
             request.setResponseTime(System.currentTimeMillis() - start);
-            request.setResponseXml(sb.toString());
 
             return request.getResponseXml();
         } catch (IOException e) {
@@ -177,35 +84,11 @@ public class Connector {
         try {
             long start = System.currentTimeMillis();
 
-            URL url = new URL(request.getServer() + restPath);
-            HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+            HttpURLConnection httpCon = createHttpURLConnection(request, restPath, "DELETE", false);
 
-            addAuthorization(httpCon, username, password);
-
-            httpCon.setRequestMethod("DELETE");
-            httpCon.setConnectTimeout(CONNECTION_TIMEOUT);
-            httpCon.setReadTimeout(READ_TIMEOUT);
-
-            request.setResponseCode(httpCon.getResponseCode());
-            request.setResponseMessage(httpCon.getResponseMessage());
-            request.clearStrings();
-
-            StringBuilder sb = new StringBuilder();
-            InputStream iStream = httpCon.getErrorStream() == null ? httpCon.getInputStream() : httpCon.getErrorStream();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(iStream, "UTF-8"));
-            String brLine = br.readLine();
-
-            while (brLine != null) {
-                sb.append(brLine);
-                request.appendString(brLine);
-                brLine = br.readLine();
-            }
-
-            br.close();
+            readInput2(request, httpCon);
 
             request.setResponseTime(System.currentTimeMillis() - start);
-            request.setResponseXml(sb.toString());
 
             return request.getResponseXml();
         } catch (IOException e) {
@@ -215,11 +98,79 @@ public class Connector {
         return null;
     }
 
-    private void addAuthorization(HttpURLConnection con, String username, String password) {
+    private HttpURLConnection createHttpURLConnection(Request request, String path, String method, boolean doOutput) throws IOException {
+        URL url = new URL(request.getServer() + path);
+        HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+
         if (username != null && password != null) {
             String login = username + ":" + password;
             String auth = "Basic " + new String(Base64.getEncoder().encode(login.getBytes()));
-            con.setRequestProperty("Authorization", auth);
+            httpCon.setRequestProperty("Authorization", auth);
         }
+
+        httpCon.setRequestMethod(method);
+        httpCon.setConnectTimeout(CONNECTION_TIMEOUT);
+        httpCon.setReadTimeout(READ_TIMEOUT);
+
+        if (doOutput) {
+            httpCon.setDoOutput(true);
+            httpCon.setRequestProperty("accept-charset", "UTF-8");
+            httpCon.setRequestProperty("content-type", "application/xml");
+
+            writeOutput(request.getXml(), httpCon);
+        }
+
+        request.setResponseCode(httpCon.getResponseCode());
+        request.setResponseMessage(httpCon.getResponseMessage());
+
+        return httpCon;
+    }
+
+    private void writeOutput(String payload, HttpURLConnection httpCon) throws IOException {
+        if (payload != null) {
+            OutputStream os = httpCon.getOutputStream();
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+
+            bw.write(payload);
+            bw.flush();
+            bw.close();
+        }
+    }
+
+    private void readInput(Request request, HttpURLConnection httpCon) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        InputStream iStream = httpCon.getErrorStream() == null ? httpCon.getInputStream() : httpCon.getErrorStream();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(iStream, "UTF-8"));
+        String brLine = br.readLine();
+
+        while (brLine != null) {
+            sb.append(brLine);
+            brLine = br.readLine();
+        }
+
+        br.close();
+
+        request.setResponseXml(sb.toString());
+    }
+
+    private void readInput2(Request request, HttpURLConnection httpCon) throws IOException {
+        request.clearStrings();
+
+        StringBuilder sb = new StringBuilder();
+        InputStream iStream = httpCon.getErrorStream() == null ? httpCon.getInputStream() : httpCon.getErrorStream();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(iStream, "UTF-8"));
+        String brLine = br.readLine();
+
+        while (brLine != null) {
+            sb.append(brLine);
+            request.appendString(brLine);
+            brLine = br.readLine();
+        }
+
+        br.close();
+
+        request.setResponseXml(sb.toString());
     }
 }
